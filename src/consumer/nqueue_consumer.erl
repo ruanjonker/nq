565c7a517c20bdab29e996d25f01aab1bf82d1c9
Ruan Jonker <ruan.jonker@gmail.com>
2012-06-24 19:51:12 +0200

    more consumer unit tests

--- src/consumer/nqueue_consumer.erl
+++ src/consumer/nqueue_consumer.erl
@@ -15,13 +15,13 @@
 
 -export([
         start_link/7,
+        get_state/1, set_state/2,
         set_fun/3, set_err_fun/3
         ]).
 
 -record(state, {queue, proc_fun, proc_args, err_fun, err_args, proc_state = paused}).
 
 -define(NAME(X), {global, {?MODULE, X}}).
-
 
 start_link(ConsId, Queue, Fun, Args, ErrFun, ErrArgs, ProcState) when is_function(Fun, 3) and is_function(ErrFun, 4) and ((ProcState == paused) or (ProcState == unpaused)) ->
     gen_server:start_link(?NAME(ConsId), ?MODULE, [Queue, Fun, Args, ErrFun, ErrArgs, ProcState], []).
@@ -32,6 +32,12 @@ set_fun(ConsId, Fun, Args) when is_function(Fun, 3) ->
 set_err_fun(ConsId, ErrFun, ErrArgs) when is_function(ErrFun, 4) ->
     gen_server:call(?NAME(ConsId), {set_err_fun, ErrFun, ErrArgs}, infinity).
     
+
+get_state(ConsId) ->
+    gen_server:call(?NAME(ConsId), get_state, infinity).
+set_state(ConsId, S) when ((S == paused) or (S == unpaused)) ->
+    gen_server:call(?NAME(ConsId), {set_state, S}, infinity).
+
 
 
 init([Queue, Fun, Args, ErrFun, ErrArgs, ProcState]) ->
@@ -46,16 +52,30 @@ init([Queue, Fun, Args, ErrFun, ErrArgs, ProcState]) ->
     {ok, #state{queue = Queue, proc_fun = Fun, proc_args = Args, err_fun = ErrFun, err_args = ErrArgs, proc_state = ProcState}}.
 
 
+handle_call({set_state, S}, _, #state{queue = Queue} = State) ->
+    case S of
+    unpaused ->
+        ok = nqueue:subscribe(Queue);
+    _ ->
+        ok = nqueue:unsubscribe(Queue)
+    end,
+    {reply, ok, State#state{proc_state = S}, 0};
+handle_call(get_state, _, #state{proc_state = P} = State) ->
+    {reply, P, State, 0};
 handle_call({set_fun, Fun, Args}, _, State) ->
-    {noreply, State#state{proc_fun = Fun, proc_args = Args}, 0};
+    {reply, ok,  State#state{proc_fun = Fun, proc_args = Args}, 0};
 handle_call({set_err_fun, Fun, Args}, _, State) ->
-    {noreply, State#state{err_fun = Fun, err_args = Args}, 0}.
+    {reply, ok, State#state{err_fun = Fun, err_args = Args}, 0}.
 
 handle_cast(_, State) ->
     {noreply, State, 0}.
-handle_info(timeout, #state{queue = Queue, proc_fun = Fun, proc_args = Args, err_fun = ErrFun, err_args = ErrArgs} = State) ->
 
+handle_info(timeout, #state{queue = Queue, proc_fun = Fun, proc_args = Args, err_fun = ErrFun, err_args = ErrArgs, proc_state = unpaused} = State) ->
+
+    T0 = now(),
     Self = self(),
+
+    ErrRetryIntervalMs = 5000,
 
     Acquisition = 
 
@@ -66,15 +86,13 @@ handle_info(timeout, #state{queue = Queue, proc_fun = Fun, proc_args = Args, err
         
     {error, not_found} ->
 
-        Ts = now(),
-
         CacheFun = fun(Q, M, _) ->
-            ok = ?dbset("consumer_cache", Self, {Q, M, Ts, 0}) 
+            ok = ?dbset("consumer_cache", Self, {Q, M, T0, 0}) 
         end,
 
         case nqueue:deq(Queue, CacheFun, undefined) of
         {ok, CachedMsg} ->
-            {ok, CachedMsg, Ts, 0};
+            {ok, CachedMsg, T0, 0};
 
         Error ->
             Error
@@ -86,29 +104,49 @@ handle_info(timeout, #state{queue = Queue, proc_fun = Fun, proc_args = Args, err
     case Acquisition of
     {ok, Message, ProcTs, ProcCount} ->
 
-        case catch(Fun(Queue, Message, Args)) of
-        ok ->
+        {MustSleep, SleepTimeMs} = 
+        if (ProcCount > 0) ->
+            DiffMs = trunc(timer:now_diff(T0, ProcTs)/1000),
 
-            ok = ?dbdel("consumer_cache", Self),
+            if (DiffMs > ErrRetryIntervalMs) ->
+                {false, 0};
+            true ->
+                {true, ErrRetryIntervalMs - DiffMs}
+            end;
 
-            {noreply, State, 0};
+        true ->
+            {false, 0}
+        end,
 
-        DeqError ->
-        
-            case catch (ErrFun(Queue, Message, DeqError, ErrArgs)) of
+        if (not MustSleep) ->
+
+            case catch(Fun(Queue, Message, Args)) of
             ok ->
-                ok = ?dbdel("consumer_cache", Self);
+    
+                ok = ?dbdel("consumer_cache", Self),
+    
+                {noreply, State, 0};
+    
+            DeqError ->
+            
+                case catch (ErrFun(Queue, Message, DeqError, ErrArgs)) of
+                ok ->
+                    ok = ?dbdel("consumer_cache", Self);
+    
+                ErrFunError ->
+    
+                    ok = ?dbset("consumer_cache", Self, {Queue, Message, ProcTs, ProcCount + 1}),                
+    
+                    error_logger:error_msg("Call to ~p(~p,~p,~p,~p) to handle message handling error (~p) failed with ~p~n", [ErrFun, Queue, Message, DeqError, ErrArgs, DeqError, ErrFunError])
+    
+                end,
+    
+                {noreply, State, ErrRetryIntervalMs}
+    
+            end;
 
-            ErrFunError ->
-
-                ok = ?dbset("consumer_cache", Self, {Queue, Message, ProcTs, ProcCount + 1}),                
-
-                error_logger:error_msg("Call to ~p(~p,~p,~p,~p) to handle message handling error (~p) failed with ~p~n", [ErrFun, Queue, Message, DeqError, ErrArgs, DeqError, ErrFunError])
-
-            end,
-
-            {noreply, State, 5000}
-
+        true ->
+            {noreply, State, SleepTimeMs}
         end;
 
     {error, empty} ->
@@ -117,6 +155,12 @@ handle_info(timeout, #state{queue = Queue, proc_fun = Fun, proc_args = Args, err
 
         {noreply, State}
     end;
+
+handle_info(timeout, #state{proc_state = paused} = State) ->
+    {noreply, State};
+handle_info({nqueue, QName, ready}, #state{proc_state = paused} = State) ->
+    ok = nqueue:unsubscribe(QName),
+    {noreply, State};
 
 handle_info(_, State) ->
     {noreply, State, 0}.
--- test/nqueue_consumer_tests.erl
+++ test/nqueue_consumer_tests.erl
@@ -1,5 +1,7 @@
 -module(nqueue_consumer_tests).
 
+-include("nq.hrl").
+
 -include_lib("eunit/include/eunit.hrl").
 
 setup_test() ->
@@ -16,12 +18,14 @@ setup_test() ->
 
     ?assertEqual({ok, 0}, bdb_store:count("consumer_cache")),
 
+%    error_logger:tty(true),
+
     ok.
 
 
 start_link_test() ->
 
-    F = fun(_Q,M,A) -> A ! M, ok end,
+    F = fun(_Q,M,A) -> {ok, S} =  bdb_store:count("consumer_cache"),  A ! {M, S}, ok end,
 
     Fe = fun(_Q,_M,_E,_A) -> ok end,
 
@@ -33,19 +37,120 @@ start_link_test() ->
 
 consume_1_test() ->
 
+    ?assertEqual(0, nqueue:size("test")),
     ?assertEqual(ok, nqueue:enq("test", hello12345)),
 
     receive M0 ->
-        ?assertEqual(M0, hello12345)
+        ?assertEqual(M0, {hello12345, 1})
 
     end,
 
-    ?assertEqual(ok, bdb_store:sync("consumer_cache")),
+    ?assertEqual(unpaused, nqueue_consumer:get_state("test")),
+
+    ?assertEqual({ok, 0}, bdb_store:count("consumer_cache")),
+    ?assertEqual(0, nqueue:size("test")),
+
+    ok.
+
+set_fun_test() ->
+
+    F = fun(_,_,A) -> A end,
+
+    ?assertEqual(ok, nqueue_consumer:set_fun("test", F, 'some error')),
+
+    ok.
+
+set_err_fun_test() ->
+
+    Fe = fun(_,M,E, A) -> A ! {M, E}, ok end,
+
+    ?assertEqual(ok, nqueue_consumer:set_err_fun("test", Fe, self())),
+
+    ok.
+
+consume_err_test() ->
+
+    ?assertEqual(0, nqueue:size("test")),
+
+    ?assertEqual(ok, nqueue:enq("test", bla)),
+
+    receive M0 ->
+        ?assertEqual({bla, 'some error'}, M0)
+    end,
+
+    ?assertEqual(0, nqueue:size("test")),
+
+    ok.
+
+consume_err_err_fun_test_() ->
+
+    {timeout, 10000, [fun() ->
+
+    Fe = fun(_,M,E, A) -> A ! {M, E}, error end,
+
+    ?assertEqual(ok, nqueue_consumer:set_err_fun("test", Fe, self())),
+
+    ?assertEqual(0, nqueue:size("test")),
+
+    ?assertEqual(ok, nqueue:enq("test", bla)),
+
+    receive M0 ->
+        ?assertEqual({bla, 'some error'}, M0)
+    end,
+
+    ?assertEqual(ok, nqueue_consumer:set_state("test", paused)),
+
+    ?assertEqual(0, nqueue:size("test")),
+    ?assertEqual({ok, 1}, bdb_store:count("consumer_cache")),
+    
+    P1 = global:whereis_name({nqueue_consumer, "test"}),
+
+    ?assertMatch({ok, {"test", bla, {_,_,_}, C}} when (C >= 1), ?dbget("consumer_cache", P1)),
+
+    ?assertEqual(ok, nqueue_consumer:set_state("test", unpaused)),
+
+    receive M1 ->
+        ?assertEqual({bla, 'some error'}, M1)
+    end,
+
+    ?assertEqual(ok, nqueue_consumer:set_state("test", paused)),
+
+    ?assertMatch({ok, {"test", bla, {_,_,_}, C}} when (C >= 2), ?dbget("consumer_cache", P1)),
+    ?assertEqual({ok, 1}, bdb_store:count("consumer_cache")),
+
+    Fe2 = fun(_,M,E, A) -> A ! {M, E}, ok end,
+
+    ?assertEqual(ok, nqueue_consumer:set_err_fun("test", Fe2, self())),
+
+    ?assertEqual(ok, nqueue_consumer:set_state("test", unpaused)),
+
+    receive M2 ->
+        ?assertEqual({bla, 'some error'}, M2)
+    end,
 
     ?assertEqual({ok, 0}, bdb_store:count("consumer_cache")),
 
+    F1 = fun(_,M,A) -> A ! M, ok end,
+    Fe3 = fun(_,_,_,_) -> ok end,
 
-    ok.
+    ?assertEqual(ok, nqueue_consumer:set_fun("test", F1, self())),
+    ?assertEqual(ok, nqueue_consumer:set_err_fun("test", Fe3, self())),
+
+    ?assertEqual(ok, nqueue:enq("test", hello12345)),
+
+    receive M3 ->
+        ?assertEqual(M3, hello12345)
+
+    end,
+
+    ?assertEqual(unpaused, nqueue_consumer:get_state("test")),
+
+    ?assertEqual({ok, 0}, bdb_store:count("consumer_cache")),
+
+    ok
+
+    end]}.
+
 
 
 teardown_test() ->
